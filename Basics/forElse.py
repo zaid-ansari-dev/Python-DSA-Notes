427ceca3c3f76ab280c9482c23ac36f8016e41b3
zaid-ansari-dev <zaid.ansari.dev@proton.me>
2025-11-21 04:56:56 +0530

    break function

---
+++ Basics/forElse.py
@@ -0,0 +1,12 @@
+successfull = True
+for number in range(3):
+    print("Attempt")
+    if successfull:
+        print("Successfull!")
+        break
+else:
+    print("unsucksexfull after 3 rounds")
+
+for x in range(5):
+    for y in range(3):
+        print(f"x: {x}, y: {y}")
