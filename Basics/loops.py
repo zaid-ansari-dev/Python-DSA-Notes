87757dcb986b8577f8468eebaf2d332c5329c0f4
zaid-ansari-dev <zaid.ansari.dev@proton.me>
2025-11-21 04:43:42 +0530

    loops using range

---
+++ Basics/loops.py
@@ -0,0 +1,9 @@
+print(3 * ".")
+for number in range(3):
+    print("Attempt", number + 1, (number + 1) * ".")
+
+for number in range(1, 4):
+    print("failed", number, number * ".")
+
+for number in range(1, 10, 2):
+    print("failed", number, number * ".")
